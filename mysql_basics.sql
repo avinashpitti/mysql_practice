73eb94f74c1bc72d27697c6c530a5c4923dba815
avinashpitti <avinash87129@gmail.com>
2026-01-20 11:41:00 +0530

    modified employee table

--- mysql_basics.sql
+++ mysql_basics.sql
@@ -23,3 +23,57 @@ values
 
 select * from employees;
 
+alter table employees
+add column 
+city varchar(20) default "bangalore";
+
+desc employees;
+
+select * from employees;
+
+update employees
+set city="hyderabad"
+where id=101;
+
+update employees
+set city="chennai"
+where id=103;
+
+alter table employees
+modify column
+name varchar(20) not null;
+
+desc employees;
+
+alter table employees 
+modify column
+salary decimal(8,2) not null;
+
+alter table employees
+add column
+age int not null ;
+
+update employees
+set age=25
+where id=101;
+
+update employees
+set age=17
+where id=102;
+
+select * from employees;
+
+update employees
+set age=42
+where id=103;
+
+update employees
+set age=19
+where id=102;
+
+alter table employees
+modify column
+age int not null check(age >=18);
+
+
+
